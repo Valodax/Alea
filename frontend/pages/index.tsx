ce2bbc58999b2c2b39ea51935012dd89e36467a0
valodax <snitzel098@gmail.com>
2023-06-15 00:27:05 -0500

    :rocket: `feat` signed in working

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,14 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+    const address = useAddress();
     return (
-        <div className={styles.container}>
-            <main className={styles.main}>
-                <ConnectWallet />
-            </main>
+        <div className="flex flex-col h-screen justify-center items-center px-8 relative">
+            <div className="absolute top-0 right-0 p-4">
+                <ConnectWallet theme="dark" />
+            </div>
+            <main className="text-center">{address ? `${address} has signed in!` : "User not signed in!"}</main>
         </div>
     );
 };
