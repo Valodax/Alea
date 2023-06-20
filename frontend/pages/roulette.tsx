d34d02ecc6df331bdc7327fed1fa18723d4fb872
valodax <snitzel098@gmail.com>
2023-06-19 23:15:47 -0500

    :rocket: `feat` table complete

--- frontend/pages/roulette.tsx
+++ frontend/pages/roulette.tsx
@@ -4,12 +4,14 @@ import Header from "../components/Header";
 import Image from "next/image";
 import wheel from "../assets/wheel.svg";
 import ball from "../assets/ball.svg";
+import table from "../assets/table.svg";
 import styles from "../styles/Roulette.module.css";
 
 const Roulette: NextPage = () => {
     const [showBall, setShowBall] = useState(false);
-    const [wheelSize, setWheelSize] = useState({ height: 600, width: 600 });
+    const [wheelSize, setWheelSize] = useState({ height: 700, width: 700 });
     const [ballSize, setBallSize] = useState({ height: 20, width: 20 });
+    const [tableSize, setTableSize] = useState({ height: 2400, width: 2400 });
 
     const handleClick = () => {
         setShowBall(true);
@@ -20,9 +22,11 @@ const Roulette: NextPage = () => {
             if (window.innerWidth < 700) {
                 setWheelSize({ height: 300, width: 300 });
                 setBallSize({ height: 10, width: 10 });
+                setTableSize({ height: 1200, width: 1200 });
             } else {
-                setWheelSize({ height: 600, width: 600 });
+                setWheelSize({ height: 700, width: 700 });
                 setBallSize({ height: 20, width: 20 });
+                setTableSize({ height: 2400, width: 2400 });
             }
         };
 
@@ -33,43 +37,51 @@ const Roulette: NextPage = () => {
     }, []);
 
     return (
-        <div className="flex flex-col h-screen justify-center items-center px-8 overflow-x-hidden relative">
+        <div className="flex flex-col h-screen justify-center items-center overflow-x-hidden">
             <Header />
-            <div id="roulette-container" className="relative">
-                {showBall && (
-                    <div className={styles.ball}>
-                        <Image
-                            priority
-                            src={ball}
-                            alt="roulette ball"
-                            height={ballSize.height}
-                            width={ballSize.width}
-                        />
-                    </div>
-                )}
-                <div className={styles.spin}>
-                    <Image
-                        priority
-                        src={wheel}
-                        alt="roulette wheel"
-                        height={wheelSize.height}
-                        width={wheelSize.width}
-                    />
-                </div>
-            </div>
-            <div>
-                {!showBall ? (
-                    <div className="mt-8">
-                        <button
-                            className="bg-black text-light-gray px-6 py-3 rounded-lg shadow-lg hover:bg-gray-800 duration-300 ease-in-out"
-                            onClick={handleClick}
+            <div className="relative flex flex-row w-full justify-between">
+                <div className="w-1/3 flex justify-center items-center pl-8 pb-6">
+                    <div id="roulette-container" className="relative z-10">
+                        <div className={showBall ? styles.ball : "hidden"}>
+                            <Image
+                                priority
+                                src={ball}
+                                alt="roulette ball"
+                                height={ballSize.height}
+                                width={ballSize.width}
+                            />
+                        </div>
+                        <div className={styles.spin}>
+                            <Image
+                                priority
+                                src={wheel}
+                                alt="roulette wheel"
+                                height={wheelSize.height}
+                                width={wheelSize.width}
+                            />
+                        </div>
+                        <div
+                            className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 ${
+                                showBall ? "invisible pointer-events-none" : ""
+                            }`}
                         >
-                            Spin!
-                        </button>
+                            <button
+                                className="bg-black text-light-gray px-6 py-5 font-semibold rounded-3xl shadow-lg hover:bg-gray-800"
+                                onClick={handleClick}
+                            >
+                                Spin!
+                            </button>
+                        </div>
                     </div>
-                ) : (
-                    ""
-                )}
+                </div>
+                <Image
+                    priority
+                    src={table}
+                    alt="table"
+                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0"
+                    height={tableSize.height}
+                    width={tableSize.width}
+                />
             </div>
         </div>
     );
