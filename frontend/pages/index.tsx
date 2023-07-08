d4e918ebb73b64e6d415c51d111bf54fee7fd324
valodax <snitzel098@gmail.com>
2023-07-08 17:58:18 -0500

    :white_check_mark: `quicksave` quicksave

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import wheel from "../assets/wheel.svg";
 import jack from "../assets/jack.svg";
+import risk from "../assets/risk.svg";
 
 const Home: NextPage = () => {
     const address = useAddress();
@@ -17,7 +18,7 @@ const Home: NextPage = () => {
             <div>
                 {address ? (
                     <div className="text-center top-1/3 mx-auto">
-                        <h1 className="text-6xl font-semibold pb-20">Available Games</h1>
+                        <h1 className="text-6xl font-semibold pb-20">All Games</h1>
                         <h3 className="text-3xl font-semibold pb-10">Singleplayer</h3>
                         <div className="pb-20 flex flex-wrap justify-center gap-20">
                             <Link href="/roulette">
@@ -56,15 +57,15 @@ const Home: NextPage = () => {
                             <Link href="/roulette">
                                 <div className="w-[13.5rem] h-[13.5rem] flex items-center justify-center hover:drop-shadow-lg text-white rounded-lg hover:text-blue-400 hover:scale-105 transition-all duration-300 ease-in-out">
                                     <div className="aspect-content flex flex-col items-center justify-center space-y-2">
-                                        <div className="h-[9rem] w-[9rem]">
+                                        <div className="h-[9rem] w-[13rem]">
                                             <Image
                                                 priority
-                                                src={wheel}
-                                                alt="roulette wheel"
+                                                src={risk}
+                                                alt="risk"
                                                 className="object-contain h-full w-full drop-shadow-lg"
                                             />
                                         </div>
-                                        <span className="font-semibold text-lg drop-shadow-lg">Risk</span>
+                                        <span className="font-semibold text-lg drop-shadow-lg">Cryptorisk</span>
                                     </div>
                                 </div>
                             </Link>
